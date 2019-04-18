86f79fadfc5096de955c7d479ad10bad62fc90ff
ahb341 <36087442+ahb341@users.noreply.github.com>
2019-04-17 20:10:06 -0400

    updated

--- static_aeroelasticity_divergence.m
+++ static_aeroelasticity_divergence.m
@@ -1,27 +1,34 @@
 %% Inputs/Constants
-l = 3; c = .5; S = c*l; % wing span (m); wing chord (m); % wing area (m^2)
-e = 0.01; % distance from aerodynamic center to elastic axis (m)
+l = 64.44; c = 14.63; S = c*l; % wing span (m); wing chord (m); % wing area (m^2)
+e = .13*c; % distance from aerodynamic center to elastic axis (m)
+t = c/10; % airfoil thickness
+J = (1/12)*c*t*(c^2+t^2); % assuming polar moment of inertia of rectangular cross section
+G = 24*10^9; % Aluminum (Pa)
+K_alpha = (pi/2)^2*G*J/l; % torsional spring stiffness (assuming = JG)
 
 rho = 1.225; % air density
-U = 1; % velocity (m/s)
+U = 224; % velocity (m/s)
 q = (1/2)*rho*U^2;
 
-alpha0 = 10; alphae = 0; % initial AoA (deg); twist due to spring (deg) 
-alpha = alpha0 + alphae; % angle of attack (deg)
+alpha0 = pi/18; alphae = 0; % initial AoA (rad); twist due to spring (rad) 
+alpha = alpha0 + alphae; % angle of attack (rad)
 
 %% Flat Plate
-CLdot = 2*pi; % dCL/dalpha for a flat plate
+CLa = 2*pi; % dCL/dalpha for a flat plate
 C_MAC0 = 0; CL0 = 0; % flat plate
 
-CL = CL0 + CLdot*alpha; % coefficient of lift
-L = CL*q*S; % lift (N)
+% CL = CL0 + CLa*alpha; % coefficient of lift
+% L = CL*q*S; % lift (N)
+% C_MAC = C_MAC0; % pitching moment coefficient about aerodynamic center
+% M_AC = C_MAC*q*S*c; % moment about aerodynamic center
+% My = M_AC + L*e - K_alpha*alphae; % moment about elastic axis or center
+[UD, qD, alphae] = divergence(K_alpha,S,e,q,CLa,rho,alpha0);
+fprintf('Divergence Velocity: %f m/s\n', UD);
+fprintf('Divergence Dynamic Pressure: %f Pa\n', qD);
+fprintf('Divergence Angle of Twist: %f deg\n', alphae*180/pi);
 
-C_MAC = C_MAC0;
-M_AC = C_MAC*q*S*c; % moment about aerodynamic center
-
-K_alpha = 1; % torsional spring stiffness
-My = M_AC + L*e - K_alpha*alphae; % moment about elastic axis or center
-
+%% Functions
+function [UD, qD, alphae] = divergence(K_alpha,S,e,q,CLa,rho,alpha0)
 % solving for alphae when My = 0 (assuming C_MAC0 = 0 for simplicity)
 % alphae = (q*S/K_alpha)*(e*CLdot*alpha0)/(1-q*(S*e/K_alpha)*CLdot);
 
@@ -30,12 +37,12 @@ My = M_AC + L*e - K_alpha*alphae; % moment about elastic axis or center
 
 % divergence dynamic pressure (solving divergence condition for q)
 % note that only for e > 0 will divergence occur
-qD = K_alpha/(S*e*CLdot);
+qD = K_alpha/(S*e*CLa);
 UD = sqrt(2*qD/rho); % divergence velocity (m/s)
 
 % condensed equation for alphae
 alphae = (q/qD)*alpha0/(1-q/qD);
+end
 
-%% Function
-[UD, qD, alphae] = divergence(K_alpha,S,e,q,CLdot)
+function [a, b] = control_reversal()
 end
